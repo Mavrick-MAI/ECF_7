3c1023c04c97a6e4e3120d522da8d1e4657ff5c2
Mavrick-MAI <112839299+Mavrick-MAI@users.noreply.github.com>
2023-08-03 15:27:02 +1100

    ajout d'un formulaire de création d'equipe dans EquipeController.php

--- app/modules/frontend/controllers/EquipeController.php
+++ app/modules/frontend/controllers/EquipeController.php
@@ -3,11 +3,16 @@ declare(strict_types=1);
 
 namespace WebAppSeller\Modules\Frontend\Controllers;
 
+use Phalcon\Forms\Element\Select;
+use Phalcon\Forms\Element\Submit;
+use Phalcon\Forms\Element\Text;
+use Phalcon\Forms\Form;
+use WebAppSeller\Models\ChefDeProjet;
+use WebAppSeller\Models\Developpeur;
 use WebAppSeller\Models\Equipe;
 
 class EquipeController extends ControllerBase
 {
-
     public function indexAction()
     {
         $equipeHTML = "";
@@ -55,7 +60,52 @@ class EquipeController extends ControllerBase
             $equipeHTML .= "</table>";
         }
 
+        $createForm = new Form();
+
+        $nomEquipeInput = new Text('nomEquipe', ['required' => true]);
+        $nomEquipeInput->setLabel('Nom équipe');
+        $createForm->add($nomEquipeInput);
+
+        $listChefName = ["Choisir un chef de projet"];
+        foreach (ChefDeProjet::find() as $chef) {
+            $chefId = $chef->getId();
+            $chefPrenomNom = $chef->Collaborateur->getPrenomNom();
+            $listChefName[] = [$chefId => $chefPrenomNom];
+        }
+
+        $chefDeProjetInput = new Select('chefDeProjet', $listChefName);
+        $chefDeProjetInput->setLabel('Chef de projet');
+        $createForm->add($chefDeProjetInput);
+
+        $listDevName = ["Choisir un développeur"];
+        foreach (Developpeur::find() as $dev) {
+            $devId = $dev->getId();
+            $devPrenomNom = $dev->Collaborateur->getPrenomNom();
+            $listDevName[] = [$devId => $devPrenomNom];
+        }
+
+        $dev1Input = new Select('dev1', $listDevName);
+        $dev1Input->setLabel('Développeur 1');
+        $createForm->add($dev1Input);
+
+        $dev2Input = new Select('dev2', $listDevName);
+        $dev2Input->setLabel('Développeur 2');
+        $createForm->add($dev2Input);
+
+        $dev3Input = new Select('dev3', $listDevName);
+        $dev3Input->setLabel('Développeur 3');
+        $createForm->add($dev3Input);
+
+        $submit = new Submit('Valider', ['class' => 'btn btn-success']);
+        $createForm->add($submit);
+
         $this->view->setVar('equipe', $equipeHTML);
+        $this->view->setVar('create', $createForm);
+    }
+
+    public function createEquipeAction()
+    {
+
     }
 
 }
