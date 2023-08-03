6471351e78d1dc709920a091ac2a5362286b7428
Mavrick-MAI <112839299+Mavrick-MAI@users.noreply.github.com>
2023-08-03 15:30:04 +1100

    ajout d'un script pour la vue des Equipe

---
+++ public/js/scriptEquipe.js
@@ -0,0 +1,29 @@
+function selectDev(event) {
+    console.log(event.value);
+    dev1Input = document.getElementsByName("dev1")[0];
+    dev2Input = document.getElementsByName("dev2")[0];
+    dev3Input = document.getElementsByName("dev3")[0];
+
+    for (let option of dev1Input.options) {
+        option.disabled = false
+        if (option.value != 0 && (option.value == dev2Input.value || option.value == dev3Input.value)) {
+            option.disabled = true
+        }
+    }
+    for (let option of dev2Input.options) {
+        option.disabled = false
+        if (option.value != 0 && (option.value == dev1Input.value || option.value == dev3Input.value)) {
+            option.disabled = true
+        }
+    }
+    for (let option of dev3Input.options) {
+        option.disabled = false
+        if (option.value != 0 && (option.value == dev1Input.value || option.value == dev2Input.value)) {
+            option.disabled = true
+        }
+    }
+}
+
+function deleteEquipe(idEquipe) {
+    console.log(idEquipe);
+}
