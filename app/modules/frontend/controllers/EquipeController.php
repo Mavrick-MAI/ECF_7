b7204ecbace635e9a169f8dea77dbf9e0e9f61bb
Mavrick-MAI <112839299+Mavrick-MAI@users.noreply.github.com>
2023-09-28 17:21:32 +1100

    commentaires + amélioration du HTML + amélioration de la gestion des erreurs

--- app/modules/frontend/controllers/EquipeController.php
+++ app/modules/frontend/controllers/EquipeController.php
@@ -14,54 +14,79 @@ use WebAppSeller\Models\Equipe;
 
 class EquipeController extends ControllerBase
 {
+    /**
+     * Action qui génére le HTML pour la page affichant toutes les équipes existantes en BDD
+     * avec possibilité de consultation, création, modification et suppression
+     */
     public function indexAction()
     {
 
+        // récupère la liste des équipes en BDD
         $equipeList = Equipe::find();
+
+        // liste des entêtes de colonnes pour les tables des équipes
         $listColumnName = ['Nom', 'Prénom', 'Niveau', 'Role'];
 
-        $htmlContent = "<div class='page-header'>";
-        $htmlContent .= "<h2>Liste de vos équipes</h2>";
-        $htmlContent .= "</div>";
+        // création du HTML de la page
+        // titre de la page
+        $htmlContent = "<h1>Liste de vos équipes</h1>";
 
+        // bouton de création d'équipe
         $htmlContent .= "<div><a class='btn btn-primary' href='equipe/createEditEquipePage'>Créer une équipe</a></div>";
 
+        // boucle sur la liste d'équipe pour créer les tables
         foreach ($equipeList as $equipe) {
 
+            // le nom de l'équipe
             $htmlContent .= "<h2 class='mt-4' style='float:left;'>".$equipe->getNom()."</h2>";
             $htmlContent .= "<div name='buttonTab'>";
-            $htmlContent .= "<a style='float:left;' class='btn mt-4' href='equipe/informationEquipePage?idEquipe=".$equipe->getId()."'><i class='fa-solid fa-eye fa-lg' style='color: #292823;'></i></a>";
-            $htmlContent .= "<a style='float:left;' class='btn mt-4' href='equipe/createEditEquipePage?idEquipe=".$equipe->getId()."'><i class='fa-regular fa-pen-to-square fa-lg' style='color: #c17d11;'></i></a>";
+            // le bouton pour consulter les informations d'une équipe
+            $htmlContent .= "<a style='float:left;' class='btn mt-4' title='Consulter cette équipe' href='equipe/informationEquipePage?idEquipe=".$equipe->getId()."'><i class='fa-solid fa-eye fa-lg' style='color: #292823;'></i></a>";
+            // le bouton pour modifier une équipe
+            $htmlContent .= "<a style='float:left;' class='btn mt-4' title='Modifier cette équipe' href='equipe/createEditEquipePage?idEquipe=".$equipe->getId()."'><i class='fa-regular fa-pen-to-square fa-lg' style='color: #c17d11;'></i></a>";
+            //
             $htmlContent .= "<div name='deleteTab'>";
-            $htmlContent .= "<button name='deleteEquipe' style='float:left;' data-id='".$equipe->getId()."' type='button' class='btn mt-4'><i class='fa-solid fa-trash-can fa-lg' style='color: #ed0707;'></i></button>";
+            // le bouton pour supprimer une équipe
+            // créer un form dans la div courante qui demande confirmation de la suppression
+            $htmlContent .= "<button name='deleteEquipe' style='float:left;' data-id='".$equipe->getId()."' type='button' class='btn mt-4' title='Supprimer cette équipe' ><i class='fa-solid fa-trash-can fa-lg' style='color: #ed0707;'></i></button>";
             $htmlContent .= "</div>";
             $htmlContent .= "</div>";
+
+            // création de la table de l'équipe courante
             $htmlContent .= "<table class='table'>";
+            // créer la ligne des entêtes de colonnes
             $htmlContent .= "<thead class='table-dark fs-5'>";
             $htmlContent .= "<tr>";
-
+            // boucle sur la liste des noms de colonnes
             foreach ($listColumnName as $colName) {
+                // créer la colonne
                 $htmlContent .= "<td>$colName</td>";
             }
-
             $htmlContent .= "</tr>";
             $htmlContent .= "</thead>";
+            // création du corps de la table
             $htmlContent .= "<tbody class='table-group-divider'>";
 
+            // récupère les informations du chef de projet dans la table Collaborateur
             $chefEquipe = $equipe->ChefDeProjet->Collaborateur;
 
-            $htmlContent .= "<tr>";
+            // créer la ligne du chef de projet en premier dans la table
+            $htmlContent .= "<tr class='table-primary'>";
             $htmlContent .= "<td>".$chefEquipe->getNom()."</td>";
             $htmlContent .= "<td>".$chefEquipe->getPrenom()."</td>";
             $htmlContent .= "<td>".$chefEquipe->getNiveauLibelle()."</td>";
-            $htmlContent .= "<td>Chef d'équipe</td>";
+            $htmlContent .= "<td>Chef d'équipe <i class='fa-solid fa-crown fa-lg' style='color: #FFD700;'></i></td>";
             $htmlContent .= "</tr>";
 
+            // boucle sur la liste des développeurs appartenant à l'équipe
             foreach ($equipe->CompositionEquipe as $dev) {
 
+                // récupère la compétence du développeur (Front, Back ou Database)
                 $devCompetence = $dev->Developpeur->getCompetenceLibelle();
+                // récupère les informations du développeur dans la table Collaborateur
                 $collab = $dev->Developpeur->Collaborateur;
 
+                // créer la ligne du développeur dans la table
                 $htmlContent .= "<tr>";
                 $htmlContent .= "<td>".$collab->getNom()."</td>";
                 $htmlContent .= "<td>".$collab->getPrenom()."</td>";
@@ -73,166 +98,236 @@ class EquipeController extends ControllerBase
             $htmlContent .= "</table>";
         }
 
+        // affecte le html à la vue
         $this->view->setVar('htmlContent', $htmlContent);
     }
 
+    /**
+     * Action qui génére le HTML pour la page permettant de créer ou de modifier une équipe en BDD
+     */
     public function createEditEquipePageAction()
     {
+        // intégre le script JS lié aux équipes pour les select des développeurs
         $this->assets->addJs(PUBLIC_PATH.'/js/scriptEquipe.js');
 
+        // créer le formulaire de création/modification d'équipe
         $createForm = new Form();
 
+        // créer le champ pour le nom de l'équipe, le champ ne peut pas être vide à l'envoi
         $nomEquipeInput = new Text('nomEquipe', ['required' => true]);
         $nomEquipeInput->setLabel('Nom équipe');
         $createForm->add($nomEquipeInput);
 
-        $listChefName = ["Choisir un chef de projet"];
+        // créer la liste des chef de projet
+        $listChefName = ['' => "Choisir un chef de projet",];
+        // boucle sur tous les chefs de projet en BDD
         foreach (ChefDeProjet::find() as $chef) {
+            // récupère l'id
             $chefId = $chef->getId();
+            // récupère le prénom et le nom
             $chefPrenomNom = $chef->Collaborateur->getPrenomNom();
-            $listChefName[] = [$chefId => $chefPrenomNom];
+            // insére le chef de projet dans la liste
+            $listChefName[$chefId] = $chefPrenomNom;
         }
 
+        // créer la liste déroulante des chefs de projet avec la liste créée au préalable
         $chefDeProjetInput = new Select('chefDeProjet', $listChefName);
         $chefDeProjetInput->setLabel('Chef de projet');
+        $chefDeProjetInput->setAttribute('id', 'chefDeProjet');
+        $chefDeProjetInput->setAttribute('required', true);
+
         $createForm->add($chefDeProjetInput);
 
-        $listDevName = ["Choisir un développeur"];
+        // créer la liste des développeurs
+        $listDevName = ['' => "Choisir un développeur"];
         foreach (Developpeur::find() as $dev) {
+            // récupère l'id
             $devId = $dev->getId();
+            // récupère le prénom et le nom
             $devPrenomNom = $dev->Collaborateur->getPrenomNom();
-            $listDevName[] = [$devId => $devPrenomNom];
+            // récupère la compétence
+            $devCompetence = $dev->getCompetenceLibelle();
+            // insére le développeur dans la liste
+            $listDevName[$devId] = $devPrenomNom." (".$devCompetence.")";
         }
 
+        // créer la liste déroulante pour le développeur 1 avec la liste créée au préalable
         $dev1Input = new Select('dev1', $listDevName, ['onchange' => 'selectDev(this)']);
         $dev1Input->setLabel('Développeur 1');
         $createForm->add($dev1Input);
 
+        // créer la liste déroulante pour le développeur 2 avec la liste créée au préalable
         $dev2Input = new Select('dev2', $listDevName, ['onchange' => 'selectDev(this)']);
         $dev2Input->setLabel('Développeur 2');
         $createForm->add($dev2Input);
 
+        // créer la liste déroulante pour le développeur 3 avec la liste créée au préalable
         $dev3Input = new Select('dev3', $listDevName, ['onchange' => 'selectDev(this)']);
         $dev3Input->setLabel('Développeur 3');
         $createForm->add($dev3Input);
 
+        // créer le bouton d'envoi du formulaire
         $submit = new Submit('Valider', ['class' => 'btn btn-success']);
         $createForm->add($submit);
 
         // récupère l'identifiant de l'équipe dans le cas d'une modification
         $idEquipe = $this->request->get('idEquipe');
+        // si l'identifiant équipe existe, il s'agit d'une modification
         if ($idEquipe != null) {
+            // récupère l'équipe en BDD à partir de l'identifiant
             $equipe = Equipe::findFirst($idEquipe);
             $nomEquipe = $equipe->getNom();
             $idChefEquipe = $equipe->getIdChefDeProjet();
 
+            // affecte le nom de l'équipe au champ du formulaire
             $nomEquipeInput->setDefault($nomEquipe);
+            // affecte le chef de projet de l'équipe au formulaire
             $chefDeProjetInput->setDefault($idChefEquipe);
 
-            $developpeurs = CompositionEquipe::find("id_equipe=".$idEquipe);
-            $idDevs = [];
-            foreach ($developpeurs as $developpeur) {
-                $idDevs[] = $developpeur->getIdDeveloppeur();
-            }
+            // récupère les développeurs liés à l'équipe à partir de l'identifiant d'équipe
+            $developpeurs = CompositionEquipe::find([
+                'conditions' => 'id_equipe = :idEquipe:',
+                'bind' => [
+                    'idEquipe' => $idEquipe,
+                ],
+            ]);
 
-            $dev1Input->setDefault($idDevs[0]);
-            $dev2Input->setDefault($idDevs[1]);
-            $dev3Input->setDefault($idDevs[2]);
+            // affecte les identifiants des développeurs aux listes déroulantes
+            $dev1Input->setDefault(!empty($developpeurs[0]) ? $developpeurs[0]->getIdDeveloppeur() : 0);
+            $dev2Input->setDefault(!empty($developpeurs[1]) ? $developpeurs[1]->getIdDeveloppeur() : 0);
+            $dev3Input->setDefault(!empty($developpeurs[2]) ? $developpeurs[2]->getIdDeveloppeur() : 0);
 
+            // créé le titre de la page pour une modification d'équipe
             $htmlContent = "<div class='page-header'>";
-            $htmlContent .= "<h2>Modification d'une équipe</h2>";
+            $htmlContent .= "<h1>Modification d'une équipe</h1>";
             $htmlContent .= "</div>";
 
-            $htmlContent .= "<form method='post' action='createEquipe?idEquipe=".$idEquipe."'>";
+            // créé la balise du formulaire qui appelle l'action de création/modification d'équipe
+            // un id d'équipe est envoyé dans le cas d'une modification
+            $htmlContent .= "<form method='post' action='createEditEquipe?idEquipe=".$idEquipe."' onsubmit='return checkForm()'>";
         } else {
 
+            // créé le titre de la page pour une création d'équipe
             $htmlContent = "<div class='page-header'>";
-            $htmlContent .= "<h2>Création d'une équipe</h2>";
+            $htmlContent .= "<h1>Création d'une équipe</h1>";
             $htmlContent .= "</div>";
 
-            $htmlContent .= "<form method='post' action='createEquipe'>";
+            // créé la balise du formulaire qui appelle l'action de création/modification d'équipe
+            // aucun id d'équipe est envoyé dans le cas d'une création
+            $htmlContent .= "<form method='post' action='createEditEquipe' onsubmit='return checkForm()'>";
 
         }
+        // boucle sur les éléments du formulaire afin de les rendre visible sur la page
         foreach ($createForm as $element) {
             $htmlContent .= "<div>";
+            // vérifie qu'il ne s'agit pas du bouton 'Valider'
             if ($element->getName() != 'Valider') {
+                // créé un label pour l'élément du formulaire courant
                 $htmlContent .= "<label for=''".$element->getName()."' class='me-3'>".$element->label()."</label>";
             }
+            // génére l'élément courant en une String pour l'afficher en HTML
             $htmlContent .= $element->render();
         }
+        // créé le bouton 'Annuler' afin de revenir à la gestion des équipes
         $htmlContent .= "<a class='btn btn-danger ms-2' href='../equipe'>Annuler</a>";
         $htmlContent .= "</div>";
         $htmlContent .= "</form>";
 
+        // envoie la variable contenant le HTML à la vue
         $this->view->setVar('htmlContent', $htmlContent);
     }
-
-    public function createEquipeAction()
+    /**
+     * Action qui permet de créer ou de modifier une équipe en BDD
+     * Dans le cas d'une modification, un identifiant d'équipe est envoyé
+     */
+    public function createEditEquipeAction()
     {
-        /* Vérifie que la requête est de type POST */
+        // initialisation du message d'erreur
+        $errorMessage = "";
+        // Vérifie que la requête est de type POST
         if ($this->request->isPost())
         {
+            // récupère l'identifiant du chef d'équipe du formulaire
             $idChefEquipe = $this->request->get('chefDeProjet');
 
-            /* Vérifie qu'un chef de projet a bien été sélectionné */
-            if ($idChefEquipe != 0)
+            // Vérifie qu'un chef d'équipe a bien été sélectionné
+            if (!empty($idChefEquipe))
             {
+                // récupère le nom de l'équipe du formulaire
                 $nomEquipe = $this->request->get('nomEquipe');
 
+                // récupère les identifiants des développeurs du formulaire
                 $idDevs[] = $this->request->get('dev1');
                 $idDevs[] = $this->request->get('dev2');
                 $idDevs[] = $this->request->get('dev3');
 
-                if ($this->request->get('idEquipe') != null) {
+                // vérifie si il s'agit d'une création ou d'une modification
+                if (!empty($this->request->get('idEquipe'))) {
+                    // si l'identifiant équipe existe dans la requête, il s'agit d'une modification
+                    // récupère l'équipe dans la base de données
                     $equipe = Equipe::findFirst($this->request->get('idEquipe'));
                 } else {
-                    /* Créer la nouvelle équipe */
+                    // si l'identifiant équipe n'existe pas dans la requête, il s'agit d'une création
+                    // Créer la nouvelle équipe
                     $equipe = new Equipe();
                 }
-
+                    // affecte les nouvelles informations de l'équipe
                     $equipe
                         ->setNom($nomEquipe)
                         ->setIdChefDeProjet($idChefEquipe);
-
+                // vérifie que l'équipe est valide
                 if ($equipe->checkEquipe($idDevs)) {
-                    /* Si l'équipe est correctement inséré en BDD */
+                    // insère/modifie l'équipe en BDD
                     if ($equipe->save())
                     {
+                        // supprime les associations de développeur à l'équipe en BDD
                         foreach ($equipe->CompositionEquipe as $compositionEquipe) {
                             $compositionEquipe->delete();
                         }
-                        /* Créer les compositions d'équipe */
+                        // boucle sur les identifiants de développeurs reçus du formulaire
                         foreach ($idDevs as $idDev)
                         {
-                            if ($idDev != 0)
+                            // vérifie qu'il s'agit bien d'un identifiant (un id ne peut être inférieur ou égal à 0)
+                            if ($idDev > 0)
                             {
+                                // créer la nouvelle association de développeur à l'équipe
                                 $compositionEquipe = (new CompositionEquipe())
                                     ->setIdEquipe($equipe->getId())
                                     ->setIdDeveloppeur($idDev);
+                                // insère les nouvelles associations de développeur à l'équipe en BDD
                                 $compositionEquipe->save();
                             }
                         }
-                        /* Redirige sur la page des équipes */
+                        // redirige sur la page des équipes
                         $this->response->redirect($this->url->get(VIEW_PATH."/equipe"));
                     }
                 } else {
-
-                    $this->flashSession->error("Au moins un des développeurs sélectionnés est déjà dans une autre équipe avec le même chef de projet.");
-                    // renvoi les informations du formulaire
-                    $this->dispatcher->forward([
-                        'action' => 'createEditEquipePage',
-                        'params' => [
-                            'formValues' => [
-                                'nomEquipe' => $nomEquipe,
-                                'chefDeProjet' => $idChefEquipe,
-                                'dev1' => $idDevs[0],
-                                'dev2' => $idDevs[1],
-                                'dev3' => $idDevs[2],
-                            ],
-                        ],
-                    ]);
+                    // message d'erreur si un des developpeur est déjà affecté à une équipe existante avec le meme chef de projet
+                    $errorMessage = "Au moins un des développeurs sélectionnés est déjà dans une autre équipe avec le même chef de projet.";
                 }
+            } else {
+                // message d'erreur si aucun chef de projet a été sélectionné
+                $errorMessage = "Un chef de projet doit être sélectionné.";
             }
+        }
+        // vérifie si le message d'erreur contient des informations
+        if (!empty($errorMessage)) {
+            // envoie le message d'erreur à la vue
+            $this->flashSession->error($errorMessage);
+            // redirige sur la page de création/modification d'une équipe
+            // renvoi aussi les informations précedemment envoyées au formulaire
+            $this->dispatcher->forward([
+                'action' => 'createEditEquipePage',
+                'params' => [
+                    'formValues' => [
+                        'nomEquipe' => $nomEquipe,
+                        'chefDeProjet' => $idChefEquipe,
+                        'dev1' => $idDevs[0],
+                        'dev2' => $idDevs[1],
+                        'dev3' => $idDevs[2],
+                    ],
+                ],
+            ]);
         }
     }
     public function informationEquipePageAction()
